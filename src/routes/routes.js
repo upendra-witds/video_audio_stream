b655c271ef735da60eb3a1a890cc5d0bd0f80e2a
upendra <upendra.negi@witds.com>
2026-02-12 12:47:31 +0530

    fix: update server port to a fixed value and add root route handler

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,5 +5,7 @@ const { videoStreaming, streamAudio } = require("../controller/videoStreaming");
 
 router.get("/audio-streaming", streamAudio);
 router.get("/video-streaming", videoStreaming);
-
+app.get("/", (req, res) => {
+  res.send("Server Connected Successfully");
+});
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 // const db = require("./config/db");
 // db();
 const app = require("./app");
-const PORT = process.env.PORT;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server  runing on 3000`);
 });
