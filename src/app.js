653bb54ade1718a96d11095c0d4b71d5b7692bf7
upendra <upendra.negi@witds.com>
2026-02-13 16:28:41 +0530

    adaptive video streaming that automatically changes the video quality

--- src/app.js
+++ src/app.js
@@ -6,5 +6,20 @@ app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(
+  "/streams",
+  express.static(path.join(__dirname, "public", "streams"), {
+    setHeaders: (res, filePath) => {
+      if (filePath.endsWith(".m3u8")) {
+        res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
+      }
+      if (filePath.endsWith(".ts")) {
+        res.setHeader("Content-Type", "video/mp2t");
+      }
+    },
+  }),
+);
+
 app.use("/api", require("./routes/routes"));
 module.exports = app;
